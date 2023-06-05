ab16e2c626a7faa726997306a5ae9685d606c6c3
CDietzel <amothwo@gmail.com>
2023-06-05 04:22:37 -0400

    updated with test motion score computation

--- compute_motion_similarity.py
+++ compute_motion_similarity.py
@@ -100,13 +100,15 @@ post_swap = [
 
 
 # UPDATE THESE PATHS
-model_output_folder = Path("/home/dietzelcc/Documents/Repos/ibc/ibc/output")
-human_motion_folder = Path("/home/dietzelcc/Documents/Repos/depthai_blazepose/outputs/")
-loss_output = Path("/home/dietzelcc/Documents/Repos/dtw_gi/output")
+model_output_folder = Path("/home/locobot/Documents/Repos/ibc/ibc/output")
+robot_test_motion_folder = Path("/home/locobot/Documents/Repos/depthai_blazepose/5DoF")
+human_motion_folder = Path("/home/locobot/Documents/Repos/depthai_blazepose/outputs/")
+loss_output = Path("/home/locobot/Documents/Repos/dtw_gi/output")
 
 robot_motion_prefix = "oracle_interbotix_"
 robot_motion_postfix = "_n"
 robot_motion_extension = ".modulated"
+robot_test_motion_extension = ".recording"
 human_motion_extension = ".pickle"
 motion_name_list = ["test1", "test2", "test3", "test4", "test5"]
 
@@ -127,12 +129,50 @@ full_nums = []
 full_tags = []
 full_names = []
 
-avg_ctw_scores = []
-avg_soft_dtw_gi_scores = []
-avg_dtw_gi_scores = []
-avg_nums = []
-avg_tags = []
-avg_names = []
+# avg_ctw_scores = []
+# avg_soft_dtw_gi_scores = []
+# avg_dtw_gi_scores = []
+# avg_nums = []
+# avg_tags = []
+# avg_names = []
+
+for name in motion_name_list:
+    i = -1
+    tag = "human"
+
+    robot_motion_path = robot_test_motion_folder / (name + robot_test_motion_extension)
+    human_motion_path = human_motion_folder / (name + human_motion_extension)
+
+    human_poses = load_pickle(human_motion_path)
+    robot_data = load_pickle(robot_motion_path)[0][::5]
+    human_data = extract_human_keypoints(human_poses)
+
+    # Only include arm keypoints (11-22)
+    human_data = human_data[:, 11:23, :]
+
+    # Reshape to flatten xyz coordinates for all keypoints for each frame
+    human_data = human_data.reshape(-1, 36)
+
+    # Ignore first few samples (noisy data)
+    human_data = human_data[3:]
+
+    ctw_loss = ctw(robot_data, human_data)
+
+
+
+
+    _, _, dtw_gi_loss = dtw_gi(robot_data, human_data) # type: ignore
+    soft_dtw_gi_loss = softdtw_gi(robot_data, human_data)
+
+    full_ctw_scores.append(ctw_loss)
+    full_dtw_gi_scores.append(dtw_gi_loss)
+    full_soft_dtw_gi_scores.append(soft_dtw_gi_loss)
+    full_nums.append(i)
+    full_tags.append(tag)
+    full_names.append(name)
+
+    print("score for num " + str(i) + "/" +name + " is: " + str(dtw_gi_loss))
+
 
 for i, tag in enumerate(
     chain.from_iterable(
@@ -144,9 +184,9 @@ for i, tag in enumerate(
         )
     )
 ):
-    temp_ctw_scores = []
-    temp_soft_dtw_gi_scores = []
-    temp_dtw_gi_scores = []
+    # temp_ctw_scores = []
+    # temp_soft_dtw_gi_scores = []
+    # temp_dtw_gi_scores = []
     robot_motion_folder = model_output_folder / str(tag) / str(i)
     for name in motion_name_list:
         robot_motion_path = robot_motion_folder / (
@@ -182,16 +222,18 @@ for i, tag in enumerate(
         full_tags.append(tag)
         full_names.append(name)
 
-        temp_ctw_scores.append(ctw_loss)
-        temp_dtw_gi_scores.append(dtw_gi_loss)
-        temp_soft_dtw_gi_scores.append(soft_dtw_gi_loss)
+        print("score for num " + str(i) + "/" +name + " is: " + str(dtw_gi_loss))
 
-    avg_ctw_scores.append(mean(temp_ctw_scores))
-    avg_dtw_gi_scores.append(mean(temp_dtw_gi_scores))
-    avg_soft_dtw_gi_scores.append(mean(temp_soft_dtw_gi_scores))
-    avg_nums.append(i)
-    avg_tags.append(tag)
-    print("score for num " + str(i) + " is: " + str(mean(temp_dtw_gi_scores)))
+        # temp_ctw_scores.append(ctw_loss)
+        # temp_dtw_gi_scores.append(dtw_gi_loss)
+        # temp_soft_dtw_gi_scores.append(soft_dtw_gi_loss)
+
+    # avg_ctw_scores.append(mean(temp_ctw_scores))
+    # avg_dtw_gi_scores.append(mean(temp_dtw_gi_scores))
+    # avg_soft_dtw_gi_scores.append(mean(temp_soft_dtw_gi_scores))
+    # avg_nums.append(i)
+    # avg_tags.append(tag)
+    # print("score for num " + str(i) + " is: " + str(mean(temp_dtw_gi_scores)))
 
 full_results = pd.DataFrame(
     {
@@ -203,51 +245,51 @@ full_results = pd.DataFrame(
         "Soft DTW-GI Scores": full_soft_dtw_gi_scores,
     }
 )
-avg_results = pd.DataFrame(
-    {
-        "Model ID": avg_nums,
-        "Model Type": avg_tags,
-        "CTW Scores": avg_ctw_scores,
-        "DTW-GI Scores": avg_dtw_gi_scores,
-        "Soft DTW-GI Scores": avg_soft_dtw_gi_scores,
-    }
-)
+# avg_results = pd.DataFrame(
+#     {
+#         "Model ID": avg_nums,
+#         "Model Type": avg_tags,
+#         "CTW Scores": avg_ctw_scores,
+#         "DTW-GI Scores": avg_dtw_gi_scores,
+#         "Soft DTW-GI Scores": avg_soft_dtw_gi_scores,
+#     }
+# )
 
 loss_output.mkdir(parents=True, exist_ok=True)
 
 full_results.to_csv(loss_output / "dtw_gi_full_results.csv", index=False)
-avg_results.to_csv(loss_output / "avg_results.csv", index=False)
+# avg_results.to_csv(loss_output / "avg_results.csv", index=False)
 
-avg_results[
-    ["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]
-] = avg_results.groupby(avg_results.index % sum_hyperparameters)[
-    ["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]
-].sum(
-    numeric_only=True
-)
+# avg_results[
+#     ["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]
+# ] = avg_results.groupby(avg_results.index % sum_hyperparameters)[
+#     ["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]
+# ].sum(
+#     numeric_only=True
+# )
 
-avg_results.drop(
-    avg_results.tail(sum_hyperparameters * (num_seeds - 1)).index, inplace=True
-)
+# avg_results.drop(
+#     avg_results.tail(sum_hyperparameters * (num_seeds - 1)).index, inplace=True
+# )
 
-avg_results[["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]] /= 3
+# avg_results[["CTW Scores", "DTW-GI Scores", "Soft DTW-GI Scores"]] /= 3
 
-avg_results.to_csv(loss_output / "avg_avg_results.csv", index=False)
+# avg_results.to_csv(loss_output / "avg_avg_results.csv", index=False)
 
-for tag_num, tag in enumerate(tag_list):
-    filtered_avg = avg_results.loc[avg_results["Model Type"] == tag]
+# for tag_num, tag in enumerate(tag_list):
+#     filtered_avg = avg_results.loc[avg_results["Model Type"] == tag]
 
-    best_ctw = filtered_avg[
-        filtered_avg["CTW Scores"] == filtered_avg["CTW Scores"].min()
-    ]
-    best_dtw_gi = filtered_avg[
-        filtered_avg["DTW-GI Scores"] == filtered_avg["DTW-GI Scores"].min()
-    ]
-    best_soft_dtw_gi = filtered_avg[
-        filtered_avg["Soft DTW-GI Scores"] == filtered_avg["Soft DTW-GI Scores"].min()
-    ]
-    best_ctw.to_csv(loss_output / ("best_ctw_" + str(tag) + ".csv"), index=False)
-    best_dtw_gi.to_csv(loss_output / ("best_dtw_gi_" + str(tag) + ".csv"), index=False)
-    best_soft_dtw_gi.to_csv(
-        loss_output / ("best_soft_dtw_gi_" + str(tag) + ".csv"), index=False
-    )
+#     best_ctw = filtered_avg[
+#         filtered_avg["CTW Scores"] == filtered_avg["CTW Scores"].min()
+#     ]
+#     best_dtw_gi = filtered_avg[
+#         filtered_avg["DTW-GI Scores"] == filtered_avg["DTW-GI Scores"].min()
+#     ]
+#     best_soft_dtw_gi = filtered_avg[
+#         filtered_avg["Soft DTW-GI Scores"] == filtered_avg["Soft DTW-GI Scores"].min()
+#     ]
+#     best_ctw.to_csv(loss_output / ("best_ctw_" + str(tag) + ".csv"), index=False)
+#     best_dtw_gi.to_csv(loss_output / ("best_dtw_gi_" + str(tag) + ".csv"), index=False)
+#     best_soft_dtw_gi.to_csv(
+#         loss_output / ("best_soft_dtw_gi_" + str(tag) + ".csv"), index=False
+#     )
